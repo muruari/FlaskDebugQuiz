01fd693bac62150096d96f573f7b3b42883a72a9
muruari <35414265+muruari@users.noreply.github.com>
2018-03-12 15:06:29 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
 app.secret_key = "unicorns"
 
@@ -8,7 +8,7 @@ app.secret_key = "unicorns"
 #Good Luck Hackers!
 ####################
 
-@app.route('/') 
+@app.route('/', methods=["get", "post"]) 
 def index(): 
     
 
@@ -28,15 +28,14 @@ def index():
     return render_template("index.html", info = session['info'])
 
 
-@app.route("/form")
+@app.route("/form", methods=["post"])
 def form():
     
     if len(request.form['FirstName']) < 1 or len(request.form['Last_Name']) < 1:
         flash("Please Complete Form")
     else:
         session['info'] = [request.form["FirstName"], request.form['Last_Name'], request.form['FaveSnack']]
-
-
+        flash("Success! Your registration was received!")
     return redirect('/')
 
 
